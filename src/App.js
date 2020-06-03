2fe49672f03be2a681a7076047f740638c43708c
Gerardo Forero <gerardo.forero@linktic.com>
2020-06-03 15:24:39 -0500

    nuevo texto 2

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload. Por GerardoF
         </p>
         <p>Este es mi nuevo sitio en netlify</p>
+        <p>Bogotá - Colombiay</p>
         <a
           className="App-link"
           href="https://reactjs.org"
